afa5a016238ffc5c542528fbbbc30f66c750b727
donners donners <donnersyt@users.noreply.github.com>
2020-12-06 07:53:20 +0000

    added root page

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  console.log("You visited /");
+  res.send("Hello World");
+});
+
 app.listen(PORT, () => console.log(`Started server on port: ${PORT}`));
